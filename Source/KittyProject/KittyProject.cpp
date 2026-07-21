f7065fb29305ecb82efc003234bed5c9cc66d382
김은서 <2023180012@tukorea.ac.kr>
2026-07-21 22:06:51 +0900

    경로수정

---
+++ Source/KittyProject/KittyProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "KittyProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, KittyProject, "KittyProject" );
