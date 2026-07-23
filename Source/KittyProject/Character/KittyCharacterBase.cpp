24bdd0c7492df51432e603616f5d22913eba5e31
김은서 <2023180012@tukorea.ac.kr>
2026-07-24 02:10:39 +0900

    캐릭터 베이스 클래스 생성

---
+++ Source/KittyProject/Character/KittyCharacterBase.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "KittyCharacterBase.h"
+
+// Sets default values
+AKittyCharacterBase::AKittyCharacterBase()
+{
+
+
+}
+
+
+
+
