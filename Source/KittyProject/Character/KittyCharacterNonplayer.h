5c98f9f3b9a88641ae118fd63997cc20bbef6a7f
oh taeseok <kbbong03@tukorea.ac.kr>
2026-07-24 15:10:09 +0900

    에셋 이름 변경

---
+++ Source/KittyProject/Character/KittyCharacterNonplayer.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/KittyCharacterBase.h"
+#include "KittyCharacterNonplayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class KITTYPROJECT_API AKittyCharacterNonplayer : public AKittyCharacterBase
+{
+	GENERATED_BODY()
+};
