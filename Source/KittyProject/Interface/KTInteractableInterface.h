30b4d3ab6864713ac8848e8c7e3fd29fa4ef1ff6
김은서 <2023180012@tukorea.ac.kr>
2026-08-01 22:30:56 +0900

    빌드 성공

--- Source/KittyProject/Interface/KTInteractableInterface.h
+++ Source/KittyProject/Interface/KTInteractableInterface.h
@@ -15,6 +15,7 @@ class UKTInteractableInterface : public UInterface
 
 /**
  * 
+ * 총, 아이템, 문 등 상호작용 가능한 클래스가 구현할 인터페이스
  */
 class KITTYPROJECT_API IKTInteractableInterface
 {
@@ -22,4 +23,11 @@ class KITTYPROJECT_API IKTInteractableInterface
 
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:
+	// 플레이어가 상호작용 키를 눌렀을 때 호출, 상호작용을 시도한 Actor(플레이어)가 전달됨
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Interaction")
+	void Interact(AActor* Interactor);
+	
+	// 화면에 표시할 상호작용 안내 문구를 반환 ex) "[F] 총 획득하기" 
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable, Category = "Interaction")
+	FText GetInteractionText() const;
 };
