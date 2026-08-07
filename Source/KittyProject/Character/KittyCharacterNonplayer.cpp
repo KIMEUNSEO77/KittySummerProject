6304624d01b9b372de930f59c409915cb90dd206
oh taeseok <kbbong03@tukorea.ac.kr>
2026-08-07 14:43:17 +0900

    방향에 따라 죽는 모션을 추가 및 죽음상태 추가

--- Source/KittyProject/Character/KittyCharacterNonplayer.cpp
+++ Source/KittyProject/Character/KittyCharacterNonplayer.cpp
@@ -3,6 +3,10 @@
 
 #include "Character/KittyCharacterNonplayer.h"
 #include "AI/KittyAIController.h"
+#include "Animation/AnimMontage.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
 
 AKittyCharacterNonplayer::AKittyCharacterNonplayer()
 {
@@ -16,6 +20,23 @@ AKittyCharacterNonplayer::AKittyCharacterNonplayer()
 void AKittyCharacterNonplayer::PostInitializeComponents()
 {
 	Super::PostInitializeComponents();
+	
+	OnTakeAnyDamage.AddDynamic(
+		this,
+		&AKittyCharacterNonplayer::OnDamaged);
+}
+
+void AKittyCharacterNonplayer::OnDamaged(AActor* DamageActor, float Damage, const UDamageType* DamageType,
+	AController* InstigatedBy, AActor* DamageCauser)
+{
+	if (bIsDead||Damage<=0.0f)
+	{
+		return;
+	}
+	
+	UAnimMontage* DeathMontage = SelectDeathMontage(InstigatedBy, DamageCauser);
+	
+	Die(DeathMontage);
 }
 
 float AKittyCharacterNonplayer::GetAIPatrolRadius()
@@ -36,4 +57,62 @@ float AKittyCharacterNonplayer::GetAIAttackRange()
 float AKittyCharacterNonplayer::GetAITurnSpeed()
 {
 	return 500.0f;
+}
+
+UAnimMontage* AKittyCharacterNonplayer::SelectDeathMontage(AController* InstigatedBy, AActor* DamageCauser) const
+{
+	const AActor* Attacker = nullptr;
+	
+	if (InstigatedBy)
+	{
+		Attacker = InstigatedBy->GetPawn();
+	}
+	if (!IsValid(Attacker))
+	{
+		Attacker = DamageCauser;
+	}
+	
+	if (!IsValid(Attacker))
+	{
+		return FrontDeathMontage;
+	}
+	
+	const FVector WorldDirection = Attacker->GetActorLocation() - GetActorLocation();
+	
+	const FVector LocalDirection = GetActorTransform().InverseTransformVectorNoScale(WorldDirection).GetSafeNormal2D();
+	
+	if (FMath::Abs(LocalDirection.X)>=FMath::Abs(LocalDirection.Y))
+	{
+		return LocalDirection.X>=0.0f ? FrontDeathMontage : BackDeathMontage;
+	}
+	
+	return LocalDirection.Y >= 0.0f ? RightDeathMontage : LeftDeathMontage;
+}
+
+void AKittyCharacterNonplayer::Die(UAnimMontage* DeathMontage)
+{
+	if (bIsDead)
+	{
+		return;
+	}
+	
+	bIsDead = true;
+	SetCanBeDamaged(false);
+	
+	if (AKittyAIController* AIController = Cast<AKittyAIController>(GetController()))
+	{
+		AIController->StopMovement();
+		AIController->ClearFocus(EAIFocusPriority::Gameplay);
+		AIController->StopAI();
+	}
+	
+	GetCharacterMovement()->StopMovementImmediately();
+	GetCharacterMovement()->DisableMovement();
+	
+	GetCapsuleComponent()->SetCollisionResponseToChannel(ECC_Pawn, ECR_Ignore);
+	
+	if (DeathMontage)
+	{
+		PlayAnimMontage(DeathMontage);
+	}
 }
--- Source/KittyProject/Character/KittyCharacterNonplayer.h
+++ Source/KittyProject/Character/KittyCharacterNonplayer.h
@@ -10,8 +10,9 @@
 /**
  * 
  */
- 
- class ATargetPoint;
+
+class UAnimMontage;
+class ATargetPoint;
  
 UCLASS()
 class KITTYPROJECT_API AKittyCharacterNonplayer : public AKittyCharacterBase, public IKittyCharacterAIInterface
@@ -31,9 +32,21 @@ public:
 		return PatrolPoints;
 	}
 	
+	bool IsDead() const
+	{
+		return bIsDead;
+	}
+	
 protected:
 	virtual void PostInitializeComponents() override;
 	
+	UFUNCTION()
+	void OnDamaged(AActor* DamageActor,
+		float Damage, 
+		const UDamageType* DamageType, 
+		AController* InstigatedBy, 
+		AActor* DamageCauser);
+
 protected:
 	virtual float GetAIPatrolRadius() override;
 	virtual float GetAIDetectRange() override;
@@ -42,10 +55,33 @@ protected:
 	
 private:
 	
-	//순찰 경로 지정을 위한 값
+	//순찰가능 여부를 판단
 	UPROPERTY(EditAnywhere, Category = AI)
 	bool bPatrolEnabled;
-	
+	//순찰 위치를 저장하는 배열
 	UPROPERTY(EditAnywhere, Category = AI)
 	TArray<TObjectPtr<ATargetPoint>> PatrolPoints;
+	//죽었는지 판단하는 값
+	UPROPERTY(EditAnywhere, Category = AI)
+	bool bIsDead;
+	//각 방향별 죽는 몽타주
+	UPROPERTY(EditDefaultsOnly, Category = "Animation|Death")
+	TObjectPtr<UAnimMontage> BackDeathMontage;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Animation|Death")
+	TObjectPtr<UAnimMontage> FrontDeathMontage;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Animation|Death")
+	TObjectPtr<UAnimMontage> LeftDeathMontage;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Animation|Death")
+	TObjectPtr<UAnimMontage> RightDeathMontage;
+
+
+private:
+	//죽을때 재생할 몽타주 선택
+	UAnimMontage* SelectDeathMontage(AController* InstigatedBy,
+		AActor* DamageCauser) const;
+	//선택된 몽타주로 죽는 모션 실행.
+	void Die(UAnimMontage* DeathMontage);
 };
