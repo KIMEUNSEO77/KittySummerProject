39d46040f4bd5c45fd0950f8391045cca5508a70
김은서 <2023180012@tukorea.ac.kr>
2026-08-13 03:40:58 +0900

    문 상호작용 성공

--- Source/KittyProject/Interaction/KTKeycardDoor.cpp
+++ Source/KittyProject/Interaction/KTKeycardDoor.cpp
@@ -4,6 +4,8 @@
 #include "Interaction/KTKeycardDoor.h"
 #include "Components/SceneComponent.h"
 #include "Components/StaticMeshComponent.h"
+#include "Components/SphereComponent.h"
+#include "Engine/Engine.h"
 
 // Sets default values
 AKTKeycardDoor::AKTKeycardDoor()
@@ -12,11 +14,44 @@ AKTKeycardDoor::AKTKeycardDoor()
 
 	SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
 	SetRootComponent(SceneRoot);
+	
+	InteractionCollision =
+	CreateDefaultSubobject<USphereComponent>(TEXT("InteractionCollision"));
+
+	InteractionCollision->SetupAttachment(SceneRoot);
+	InteractionCollision->SetSphereRadius(100.0f);
+	InteractionCollision->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	InteractionCollision->SetCollisionObjectType(ECC_WorldDynamic);
+	InteractionCollision->SetCollisionResponseToAllChannels(ECR_Overlap);
+	InteractionCollision->SetGenerateOverlapEvents(true);
 
 	DoorMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DoorMesh"));
 	DoorMesh->SetupAttachment(SceneRoot);
 
 	// 플레이어와 총의 Line Trace가 문을 통과하지 않도록 설정
 	DoorMesh->SetCollisionProfileName(TEXT("BlockAll"));
+}
+
+void AKTKeycardDoor::Interact_Implementation(AActor* Interactor)
+{
+	if (!IsValid(Interactor))
+	{
+		return;
+	}
+
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(
+			-1,
+			2.0f,
+			FColor::Green,
+			TEXT("키카드 문 상호작용 성공")
+		);
+	}
+}
+
+FText AKTKeycardDoor::GetInteractionText_Implementation() const
+{
+	return FText::FromString(TEXT("[F] 출입증 사용하기"));
 }
 
--- Source/KittyProject/Interaction/KTKeycardDoor.h
+++ Source/KittyProject/Interaction/KTKeycardDoor.h
@@ -4,10 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "Interface/KTInteractableInterface.h"
 #include "KTKeycardDoor.generated.h"
 
 UCLASS()
-class KITTYPROJECT_API AKTKeycardDoor : public AActor
+class KITTYPROJECT_API AKTKeycardDoor : public AActor, public IKTInteractableInterface
 {
 	GENERATED_BODY()
 	
@@ -19,8 +20,18 @@ protected:
 	// 문틀처럼 움직이지 않는 부분
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Door|Components")
 	TObjectPtr<USceneComponent> SceneRoot;
+	
+	// 플레이어의 상호작용 탐색에 감지될 전용 충돌체
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Door|Components")
+	TObjectPtr<class USphereComponent> InteractionCollision;
 
 	// 실제로 열릴 문 메시
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Door|Components")
 	TObjectPtr<UStaticMeshComponent> DoorMesh;
+	
+	// F키를 눌렀을 때 실행되는 상호작용
+	virtual void Interact_Implementation(AActor* Interactor) override;
+
+	// 플레이어가 문을 바라볼 때 표시할 안내 문구
+	virtual FText GetInteractionText_Implementation() const override;
 };
