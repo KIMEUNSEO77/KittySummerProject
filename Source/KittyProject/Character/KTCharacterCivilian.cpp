1d40c986cc7bec44aa88c9d58d61d272da6ece46
김은서 <2023180012@tukorea.ac.kr>
2026-09-02 23:35:42 +0900

    블루프린트마다..설정

--- Source/KittyProject/Character/KTCharacterCivilian.cpp
+++ Source/KittyProject/Character/KTCharacterCivilian.cpp
@@ -2,14 +2,53 @@
 
 
 #include "Character/KTCharacterCivilian.h"
-
 #include "Animation/AnimSequence.h"
 #include "Components/SkeletalMeshComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "TimerManager.h"
 
 AKTCharacterCivilian::AKTCharacterCivilian()
 {
 	PrimaryActorTick.bCanEverTick = false;
+}
+
+void AKTCharacterCivilian::TriggerStartledReaction()
+{
+	if (bHasReacted || !IsValid(GetMesh()) || !IsValid(StartledAnimation))
+	{
+		return;
+	}
+
+	bHasReacted = true;
+
+	// 놀람 애니메이션 재생
+	GetMesh()->PlayAnimation(StartledAnimation, false);
+
+	// 설정된 음성 중 하나를 무작위로 재생
+	if (!AlertSounds.IsEmpty())
+	{
+		const int32 RandomIndex = FMath::RandRange(0, AlertSounds.Num() - 1);
+		USoundBase* SelectedSound = AlertSounds[RandomIndex];
+
+		if (IsValid(SelectedSound))
+		{
+			UGameplayStatics::PlaySoundAtLocation(
+				this,
+				SelectedSound,
+				GetActorLocation()
+			);
+		}
+	}
+
+	// 기존 생활 애니메이션으로 복귀
+	GetWorldTimerManager().SetTimer(
+		ReactionAnimationTimer,
+		this,
+		&AKTCharacterCivilian::ResumeAmbientAnimation,
+		StartledAnimation->GetPlayLength(),
+		false
+	);
 }
 
 void AKTCharacterCivilian::BeginPlay()
@@ -24,6 +63,14 @@ void AKTCharacterCivilian::BeginPlay()
 	if (IsValid(GetMesh()) && IsValid(AmbientAnimation))
 	{
 		GetMesh()->SetAnimationMode(EAnimationMode::AnimationSingleNode);
+		GetMesh()->PlayAnimation(AmbientAnimation, bLoopAmbientAnimation);
+	}
+}
+
+void AKTCharacterCivilian::ResumeAmbientAnimation()
+{
+	if (IsValid(GetMesh()) && IsValid(AmbientAnimation))
+	{
 		GetMesh()->PlayAnimation(AmbientAnimation, bLoopAmbientAnimation);
 	}
 }
--- Source/KittyProject/Character/KTCharacterCivilian.h
+++ Source/KittyProject/Character/KTCharacterCivilian.h
@@ -16,6 +16,9 @@ class KITTYPROJECT_API AKTCharacterCivilian : public AKittyCharacterBase
 	
 public:
 	AKTCharacterCivilian();
+	
+	UFUNCTION(BlueprintCallable, Category = "Civilian|Reaction")
+	void TriggerStartledReaction();
 
 protected:
 	virtual void BeginPlay() override;
@@ -27,4 +30,20 @@ protected:
 	// 애니메이션을 반복할지 여부
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Civilian|Ambient")
 	bool bLoopAmbientAnimation = true;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Civilian|Reaction")
+	TObjectPtr<class UAnimSequence> StartledAnimation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Civilian|Reaction")
+	TArray<TObjectPtr<class USoundBase>> AlertSounds;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Civilian|Reaction")
+	float DetectionRange = 1200.0f;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Civilian|Reaction")
+	bool bHasReacted = false;
+	
+	void ResumeAmbientAnimation();
+
+	FTimerHandle ReactionAnimationTimer;
 };
