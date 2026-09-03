22dd70e08ff3faefc0726a4999bff4f1b04a5ea9
김은서 <2023180012@tukorea.ac.kr>
2026-09-03 19:04:11 +0900

    ??

--- Source/KittyProject/AI/KittyAIController.h
+++ Source/KittyProject/AI/KittyAIController.h
@@ -54,7 +54,8 @@ private:
 	float SightRadius = 1200.0f;
 	
 	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category = "AI|Sight",meta = (AllowPrivateAccess = "true", ClampMin = "0.0"))
-	float LoseSightRadius = 1500.0f;
+	float LoseSightRadius = 150
+	0.0f;
 	
 	UPROPERTY(EditDefaultsOnly,BlueprintReadOnly,Category = "AI|Sight",meta = (AllowPrivateAccess = "true",ClampMin = "0.0",ClampMax = "180.0"))
 	float PeripheralVisionHalfAngle = 65.0f;
